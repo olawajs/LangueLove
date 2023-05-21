456ebf8abb45256c7ec27ec1afe04ece02178593
Alex98 <aleksandrawajs@interia.pl>
2023-05-21 21:12:08 +0200

    poprawa guzika

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,7 +61,7 @@
                                 <a class="nav-link" href="{{ route('about') }}">O nas</a>
                             </li>
                             <li class="nav-item consultation">
-                                <a class="nav-link" style="color: var(--bs-primary);" href="{{ route('consultation') }}">Darmowa konsultacja</a>
+                                <a class="nav-link" style="color: var(--bs-primary); border: 2px solid var(--bs-primary); padding: 1px; border-radius: 6px;" href="{{ route('consultation') }}">Darmowa konsultacja</a>
                             </li>
                             <!-- Authentication Links -->
                             @guest
