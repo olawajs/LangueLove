e232eac8182c484f3b1c2bdccf81b0efb30f6147
Alex98 <73954825+olawajs@users.noreply.github.com>
2024-01-03 18:57:43 +0100

    8

--- resources/views/test/lector.blade.php
+++ resources/views/test/lector.blade.php
@@ -143,7 +143,7 @@
                                    
                                 </div> 
                             </div> 
-                            @if($lector->id == 15 && $loop->index==7)
+                            @if($lector->id == 15 && $loop->index==8)
                                 @php dd('Strona chwilowo niedostępna2');  @endphp
                             @endif
                             @endforeach
